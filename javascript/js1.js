d3215072410bf7f3a23a9644f862b143504a86a3
amin kiyani <148985615+aminkiyani99@users.noreply.github.com>
2023-11-01 15:13:56 +0330

    html-css-learn

---
+++ javascript/js1.js
@@ -0,0 +1,39 @@
+// var Name,LastName,number;
+// Name="amin"
+// LastName="kiyani"
+// number="24"
+
+// document.write(Name);
+// console.log(Name,LastName,number)
+// window.alert(Name);
+
+// var a,b,c;
+// a = 12;
+// b = 8;
+// // c = a-b;
+// // c = a*b;
+// // c = a/b;
+// // c = a+b;
+// c = a%b;
+
+
+// document.write(c);
+
+// // jalase 2:
+// var number= 24; //number
+// var number2=24;
+// var name="amin"; //string
+// var variable = " codeyad "+ 24;// multiple variable
+// var person={ FirstName:"amin",LastName:"kiyani"}; //object
+
+
+
+
+// var iscomplete=false;
+// var isequal;
+
+// isequal= (number == number2); //boolean
+
+// document.write(isequal);
+
+// jalase 3:
