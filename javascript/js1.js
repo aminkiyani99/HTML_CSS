95a05570698bf23437b6ad8fc500b406efed8cc0
amin kiyani <148985615+aminkiyani99@users.noreply.github.com>
2023-11-01 17:26:32 +0330

    jalase 4

--- javascript/js1.js
+++ javascript/js1.js
@@ -1,3 +1,7 @@
+
+
+
+// ######jalase aval:########
 // var Name,LastName,number;
 // Name="amin"
 // LastName="kiyani"
@@ -19,7 +23,7 @@
 
 // document.write(c);
 
-// // jalase 2:
+// // #######jalase 2: ########
 // var number= 24; //number
 // var number2=24;
 // var name="amin"; //string
@@ -36,4 +40,77 @@
 
 // document.write(isequal);
 
-// jalase 3:
+// ########### jalase 3:arrays: ########
+
+// var names =new Array("amin", "ali", "reza", "mehdi", "mostafa");
+// console.log(names [1]);
+
+// var MyArray = ["ali", 24, true, "hoseini", 24.321312]
+// console.log(MyArray)
+
+// var colors = ["red", "blue", "black", "green", "white"];
+// console.log(colors);
+// // colors.reverse();
+// // colors.shift();
+// // colors.shift();
+// // colors.unshift("purple");
+// // colors.pop("pink");
+// // colors.push("brown");
+// // var NewColors = colors.slice();
+// // var result = colors .indexOf("blue",0);
+// var ArrayString = colors.join("-");
+// console.log(ArrayString);
+
+//#################### jalase4:if: ###########################
+
+// var a,b;
+// var isequal;
+// a=4;
+// b=3;
+
+//             // if (a > b | a ==3 ) | be mani "or" va & be mani "and" 
+// if(a > b & b == 3)
+// {
+//     isequal= true;
+    
+// }else
+// {
+//     isequal=false;
+
+// }
+
+// // a==b ? isequal=true : isequal=false;
+// console.log("the equal match is: "+ isequal);
+
+// ######################### jalase 5 :switch! #####################
+
+// var today= new Date().getDay();
+// var day;
+// switch(today)
+// {
+//     case 0:
+//         day="sunday"
+//         break;
+//     case 1:
+//         day="monday"
+//         break;
+//     case 2:
+//         day="tuesday"
+//         break;
+//     case 3:
+//         day="wednsday"
+//         break;
+//     case 4:
+//         day="thursday"
+//         break;
+//     case 5:
+//         day="friday"
+//         break;
+//     case 6:
+//         day="saturday"
+//         break
+//         default:
+//             day="your value not found !!!"
+
+// }
+// document.write("today is: " + day)
