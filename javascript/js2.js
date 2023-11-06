12edb7d69ca6d22d336f8bd39cf9a1ed90fb82c1
amin kiyani <148985615+aminkiyani99@users.noreply.github.com>
2023-11-06 13:32:16 +0330

    6/11/2023

---
+++ javascript/js2.js
@@ -0,0 +1,21 @@
+// #############3jalase 13: DOM and BOM ##########
+
+// document.getElementById("test").innerHTML="Hello DOM ";
+
+// const element = document.getElementById("test");
+// // element.innerHTML="Hello DOM 2";
+// element.innerHTML="Date: " + Date();
+
+
+// let x = 5;
+// let y = 10;
+// let
+ 
+// z
+//  = x + y;
+// alert
+// (z);
+
+<button 
+onclick
+="alert('Hello')">Click me.</button>
