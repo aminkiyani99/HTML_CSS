35149e9fed410b2ab4959abf19d2038f8bda67d4
amin kiyani <148985615+aminkiyani99@users.noreply.github.com>
2023-11-06 14:19:17 +0330

    jalase 19

--- javascript/js2.js
+++ javascript/js2.js
@@ -16,6 +16,30 @@
 // alert
 // (z);
 
-<button 
-onclick
-="alert('Hello')">Click me.</button>
+// <button 
+// onclick
+// ="alert('Hello')">Click me.</button>
+
+// ###########3 jalase 19: add element to DOM ########
+
+const names=["mohamad", "ali", "amin", "reza", "nazanin"];
+
+
+function CreateLiElement()
+{
+    for (const items of names) 
+    {
+        document.getElementsByClassName
+
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(items);
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+
+    }
+}
+
+CreateLiElement();
+document.getElementById("myList")
+.style.color
+ = "red";
