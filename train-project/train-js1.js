cabc10b63f6784cf7747208cfb8f4f86c0f18aec
amin kiyani <148985615+aminkiyani99@users.noreply.github.com>
2023-11-29 19:22:57 +0330

    alpine-axios

--- train-project/train-js1.js
+++ train-project/train-js1.js
@@ -7,4 +7,39 @@ document.addEventListener('DOMContentLoaded', function () {
     var instances = M.Sidenav.init(elems, {
         edge: "right"
     });
+
+
+
+    // setTimeout(function () {
+    //     console.log("start...");
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then((response) => response.json())
+    //         .then((json) => {
+
+    //             console.log("end!");
+    //             console.log(json)
+
+    //             let html = ""
+
+    //             for (const user of json) {
+    //                 html = html + `<tr>
+    //                 <td>${user.id}</td>
+    //                 <td>${user.name}</td>
+    //                 <td>${user.username}</td>
+    //                 <td>${user.email}</td>
+    //                 <td>${user.address.city}</td>
+    //                 <td>
+    //                     <i class="material-icons orange-text darken-2 m-l-5 m-r-5 hov-pointer">edit</i>
+    //                     <i class="material-icons red-text darken-2 m-l-5 m-r-5 hov-pointer">delete_forever</i>
+    //                 </td>
+    //             </tr>`
+
+    //                 document.querySelector(".desktop_tableb_body").innerHTML = html
+
+
+    //             }
+
+    //         }, 5000)
+    // });
+
 });
--- train-project/train-js2.js
+++ train-project/train-js2.js
@@ -13,4 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector(".add-user-modal-back").classList.add("dis-none");
 
     })
+
+
 });
