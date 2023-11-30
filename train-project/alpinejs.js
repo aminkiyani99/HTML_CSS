031f2eca4d0f08653b1f24f8feee281b5906fae4
amin kiyani <148985615+aminkiyani99@users.noreply.github.com>
2023-11-30 19:52:42 +0330

    peymayesh safahat

--- train-project/alpinejs.js
+++ train-project/alpinejs.js
@@ -1,11 +1,49 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', () => ({
         users: [],
+        pageUsers: [],
+        isloading: false,
+        showAddModal: false,
+        pageCount: 1,
+        itemsCount: 4,
+        currentPage: 1,
+
+
         getUsers() {
+            this.isloading = true
             axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
                 this.users = res.data
-                console.log(res);
+                this.pagination()
+
+
+            }).finally(() => {
+                this.isloading = false
             })
+        },
+        pagination() {
+            this.pageCount = Math.ceil(this.users.length / this.itemsCount) //10 / 4 = 3
+            let start = (this.currentPage * this.itemsCount) - this.itemsCount //0
+            let end = this.currentPage * this.itemsCount // 4
+            this.pageUsers = this.users.slice(start, end)
+            console.log(this.pageUsers)
+
+        },
+
+        nextPage() {
+            this.currentPage++
+            if (this.currentPage > this.pageCount) {
+                this.currentPage = this.pageCount
+            }
+            this.pagination()
+        },
+        prevPage() {
+            this.currentPage--
+
+            if (this.currentPage < 1) {
+                this.currentPage = 1
+            }
+            this.pagination()
+
         }
 
     }))
