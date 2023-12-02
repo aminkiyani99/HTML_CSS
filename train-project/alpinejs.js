d70c58b5c5f64165f232a94a444ae62d60782eda
amin kiyani <148985615+aminkiyani99@users.noreply.github.com>
2023-12-02 17:37:55 +0330

    add users complete

--- train-project/alpinejs.js
+++ train-project/alpinejs.js
@@ -1,18 +1,27 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', () => ({
         users: [],
+        mainUsers: [],
         pageUsers: [],
         isloading: false,
         showAddModal: false,
         pageCount: 1,
         itemsCount: 4,
         currentPage: 1,
+        searchChar: "",
+        newUserInfo: {
+            name: "",
+            username: "",
+            email: "",
+        },
+
 
 
         getUsers() {
             this.isloading = true
             axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
                 this.users = res.data
+                this.mainUsers = res.data
                 this.pagination()
 
 
@@ -44,10 +53,51 @@ document.addEventListener('alpine:init', () => {
             }
             this.pagination()
 
-        }
+        },
+        handleChangeItemsCount(value) {
+            this.currentPage = 1
+            if (value < 1) this.itemsCount = 1
+            if (value > this.users.length) this.itemsCount = this.users.length
+        },
+
+        handleSearch(value) {
+
+            this.users = this.mainUsers.filter(user => (user.name.includes(value) || user.username.includes(value) || user.email.includes(value)))
+            this.currentPage = 1
+            this.pagination()
+        },
+
+        handleSubmitAddUsersForm() {
+            this.isloading = true
+            axios.post("https://jsonplaceholder.typicode.com/users", this.newUserInfo).then((res) => {
+
+                if (res.status == 201) {
+                    this.mainUsers.push(res.data)
+                    this.showAddModal = false
+                    this.handleResetForm()
+
+                    this.pagination()
+
+                    // 'rounded' is the class I'm applying to the toast
+                    M.toast({
+                        html: '!کاربر با موفقیت ایجاد شد',
+                        classes: 'rounded green'
+                    });
+
+                }
+            }).finally(() => {
+                this.isloading = false
+
+            })
+        },
+
+        handleResetForm() {
+            this.newUserInfo = {
+                name: "",
+                username: "",
+                email: "",
+            }
+        },
 
     }))
-
-
-
 })
